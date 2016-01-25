45e8b60f5554b0aa991689e7da8f5f25e0cc3294
a-back <a-back@list.ru>
2016-01-25 19:52:46 +0000

    4 version

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("127.0.0.1", 2222))
+s.bind(("0.0.0.0", 2222))
 s.listen(1)
+#print "listen"
 while True:
     conn, addr = s.accept()
+#    print "Socket in accept", addr
     while True:
+#        print "Read for data"
         data = conn.recv(1024)
         if not data: break
+        if str(data).lower() == str("close"): 
+            conn.close()
+#            print "Connection closed"
+            break
         conn.send(data)
-        print data 
-    if data == "Close": conn.close() 
+#        print "Send", data
+    
+        
+        
